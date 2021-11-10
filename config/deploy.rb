850baca94454ee8efe5fc64ec25861f39339c731
nilun132 <nilundone132@gmail.com>
2021-11-10 14:07:21 +0700

    Set up Puma, Nginx & Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
 # Change these
-server '178.62.88.94', port: 7171, roles: [:web, :app, :db], primary: true
+server '150.95.81.117', port: 22, roles: [:web, :app, :db], primary: true
 
-set :repo_url,        'git@github.com:sheharyarn/testapp_rails.git'
+set :repo_url,        'git@github.com:nilun132/testapp_rails.git'
 set :application,     'testapp'
-set :user,            'deploy'
+set :user,            'nilun'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
